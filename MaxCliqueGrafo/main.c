78915c25e348a29e3095a1520289af5f8ef01fa5
Luis Alho <luisalhopt@gmail.com>
2016-09-08 20:19:38 +0100

    Initial Commit

---
+++ MaxCliqueGrafo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MaxCliqueGrafo
+//
+//  Created by Luis Alho on 08/09/16.
+//  Copyright © 2016 Luis Alho. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
