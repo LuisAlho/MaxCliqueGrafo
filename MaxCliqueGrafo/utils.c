b3c79c90f342ba17f3db26a2a43af74bb3538167
Luis Alho <luisalhopt@gmail.com>
2016-09-11 20:41:10 +0100

    add utils

---
+++ MaxCliqueGrafo/utils.c
@@ -0,0 +1,9 @@
+//
+//  utils.c
+//  MaxCliqueGrafo
+//
+//  Created by Luis Alho on 11/09/16.
+//  Copyright © 2016 Luis Alho. All rights reserved.
+//
+
+#include "utils.h"
---
+++ MaxCliqueGrafo/utils.h
@@ -0,0 +1,14 @@
+//
+//  utils.h
+//  MaxCliqueGrafo
+//
+//  Created by Luis Alho on 11/09/16.
+//  Copyright © 2016 Luis Alho. All rights reserved.
+//
+
+#ifndef utils_h
+#define utils_h
+
+#include <stdio.h>
+
+#endif /* utils_h */
